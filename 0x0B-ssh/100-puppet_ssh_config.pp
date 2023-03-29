ef543a20189a0c79ce0e35dbc6b1d7f0361bd0bb
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-29 23:42:49 +0100

    new final

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Client configuration file with puppet
 
-file_line { 'private_key':
-  path   => '/etc/ssh/ssh_config',
-  line   => '	HostKey ~/.ssh/school'
+file_line { 'Change the main private key':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
 }
 
-file_line { 'password_auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => '	PasswordAuthentication no'
+file_line { 'No Authenticate with passowrd':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
 }
