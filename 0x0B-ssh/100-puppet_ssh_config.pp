c09e2b852e0dda761f3d5813da2f5bf14d048deb
Chisom Daniel <chinwezechisom@gmail.com>
2023-03-29 23:41:15 +0100

    new

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,11 @@
 # Client configuration file with puppet
 
 file_line { 'private_key':
-  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '	HostKey ~/.ssh/school'
 }
 
 file_line { 'password_auth':
-  ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '	PasswordAuthentication no'
 }
