5b1d6a1d897d66f32935b5f7a5e55cf4b22ee348
Chisom Daniel <chinwezechisom@gmail.com>
2023-05-22 21:25:20 +0100

    task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''Gets data in JSON format form a url and display it '''
+import csv
+import requests
+import sys
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) == 1:
+        exit()
+
+    users_info = requests.get("https://jsonplaceholder.typicode.com/users")
+    users_info = users_info.json()
+    tasks = requests.get("https://jsonplaceholder.typicode.com/todos")
+    tasks = tasks.json()
+    userid_str = sys.argv[1]
+    user_id = int(sys.argv[1])
+
+    username = ""
+    for user in users_info:
+        if user['id'] == user_id:
+            username = user['username']
+            break
+
+    filename = "{}.csv".format(userid_str)
+    with open(filename, 'w', newline='') as f:
+        data_handler = csv.writer(f, delimiter=',')
+        for task in tasks:
+            if task['userId'] == user_id:
+                info = [userid_str, username, task['completed'], task['title']]
+                data_handler.writerow(info)
